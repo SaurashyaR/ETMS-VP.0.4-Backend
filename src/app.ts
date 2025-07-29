75e356584c07d9149d268259ed006e21ac938ad7
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-07-29 15:55:16 +0545

    final setup

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!! Server is Here!!");
